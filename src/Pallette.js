47a6371c4a46359ecf0283a5b5fd6704457347ba
Eric Kabira <erickabira@gmail.com>
2023-03-01 16:42:23 +0300

    adding the color pallette

---
+++ src/Pallette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Pallette extends Component {
+  render() {
+    return (
+      <div className='Palette'>
+        {/* navbar */}
+        <div className='pallette-colors'>
+            {/* bunch of colors */}
+        </div>
+        {/* footer */}
+      </div>
+    )
+  }
+}
